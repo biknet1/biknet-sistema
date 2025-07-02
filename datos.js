ff5f59088654158801ea3635244a848eb6726a9a
biknet1 <biknet.autopartes@gmail.com>
2025-07-02 20:17:28 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,15 +1,38 @@
 const productos = [
-  {codigo: 'Z9Y', descripcion: 'BUJIA DE ENCENDIDO MOTOS 110/125/250 (CHAMPION) Precio Mercado Libre', stock: 7, ubicacion: 'LOCAL, MOSTRADOR 1, CAJON 2.', precio: ''},
-  {codigo: '36862EX', descripcion: 'KIT SOPORTES RADIADOR FOX/SURAN/GOL TREND EXTERIOR', stock: 2, ubicacion: 'DEPTO 2, PASILLO1,ESTANTERIA 1, ESTANTE C CAJA N° 15', precio: ''},
-  {codigo: '36862', descripcion: 'KIT SOPORTES RADIADOR FOX/SURAN/GOL TREND', stock: 2, ubicacion: 'DEPTO 2, PASILLO 1, ESTANTERIA 1 CAJA 15', precio: ''},
-  {codigo: '22204-30030', descripcion: 'CAUDALIMETRO SENSOR MAF TOYOTA HILUX/ SW4/', stock: 1, ubicacion: 'DEPTO 2, PASILLO 1, ESTANTERIA 1, ESTANTE D,CAJA N°22', precio: ''},
-  {codigo: 'FUE950-32A', descripcion: 'FUELLE UNIVERSAL CHICO / LIVIANOS SEMIEJE KIT CON ABRASADERAS Y GRASA', stock: 2, ubicacion: 'LOCAL , OFICINA ESTANTERIA E 0', precio: ''},
-  {codigo: 'BOMB-PERIT', descripcion: 'BOMBA PERITA CEBADOR MANUAL 90 GRADOS', stock: 2, ubicacion: 'DEPTO 2, COCINA, ALACENA', precio: ''},
-  {codigo: 'VA-204', descripcion: 'BOMBA LAVA PARABRISAS TIPO FIAT', stock: 1, ubicacion: 'DEPTO 2, COCINA, ALACENA', precio: ''},
-  {codigo: 'RGUG100', descripcion: 'BOMBA CEBADOR MANUAL UNIVERSAL PARA MOTORES DIESEL (PERITA) PICO CORTO', stock: 2, ubicacion: 'DEPTO 2, COCINA, ALACENA', precio: ''},
-  {codigo: 'VA-201', descripcion: 'BOMBA LAVA PARABRISAS FORD 405', stock: 1, ubicacion: 'DEPTO 2, COCINA, ALACENA', precio: ''},
-  {codigo: 'VA-027/212', descripcion: 'BOMBA LAVA PARABRISAS TIPO PEUGEOT 504', stock: 1, ubicacion: 'DEPTO 2, COCINA, ALACENA', precio: ''},
-  // ... (más productos)
-  // Copiá todo el contenido desde tu archivo APP CORREGIDA.js
-  // y asegurate de cerrar el array al final como se muestra abajo
+  {
+    codigo: 'Z9Y',
+    descripcion: 'BUJIA DE ENCENDIDO MOTOS 110/125/250 (CHAMPION) Precio Mercado Libre',
+    stock: 7,
+    ubicacion: 'LOCAL, MOSTRADOR 1, CAJON 2.',
+    precio: ''
+  },
+  {
+    codigo: '36862EX',
+    descripcion: 'KIT SOPORTES RADIADOR FOX/SURAN/GOL TREND EXTERIOR',
+    stock: 2,
+    ubicacion: 'DEPTO 2, PASILLO1,ESTANTERIA 1, ESTANTE C CAJA N° 15',
+    precio: ''
+  },
+  {
+    codigo: '36862',
+    descripcion: 'KIT SOPORTES RADIADOR FOX/SURAN/GOL TREND',
+    stock: 2,
+    ubicacion: 'DEPTO 2, PASILLO 1, ESTANTERIA 1 CAJA 15',
+    precio: ''
+  },
+  {
+    codigo: '22204-30030',
+    descripcion: 'CAUDALIMETRO SENSOR MAF TOYOTA HILUX/ SW4/',
+    stock: 1,
+    ubicacion: 'DEPTO 2, PASILLO 1, ESTANTERIA 1, ESTANTE D,CAJA N°22',
+    precio: ''
+  },
+  {
+    codigo: 'FUE950-32A',
+    descripcion: 'FUELLE UNIVERSAL CHICO / LIVIANOS SEMIEJE KIT CON ABRASADERAS Y GRASA',
+    stock: 2,
+    ubicacion: 'LOCAL , OFICINA ESTANTERIA E 0',
+    precio: ''
+  }
+  // ... Agregá acá todos los productos que siguen
 ];
