2480b5916dfc6679e38b026f8a6e2990569a2a31
biknet1 <biknet.autopartes@gmail.com>
2025-07-02 15:40:30 -0300

    Update datos.js

--- datos.js
+++ datos.js
@@ -1,22 +1,24 @@
-
 const productos = [
-  {codigo: 'WP19930', descripcion: 'PASTILLA DE FRENO DELANTERO FIAT UNO/VIVACE', stock: 2, ubicacion: '', precio: ''},
-  {codigo: 'WP18350P', descripcion: 'PASTILLA DE FRENO DELANTERO TOYOTA ETIOS', stock: 12, ubicacion: '', precio: ''},
-  {codigo: 'WP16330P', descripcion: 'PASTILLA DE FRENO TRASERO COROLLA', stock: 6, ubicacion: '', precio: ''},
-  {codigo: 'WP9900', descripcion: 'PASTILLA PEUGEOT/CITROEN', stock: 3, ubicacion: '', precio: ''},
-  {codigo: '2000/2', descripcion: 'TOPE CAPOT VW POLO', stock: 10, ubicacion: '', precio: ''},
-  {codigo: '2000/5', descripcion: 'TOPE CAPOT PEUGEOT', stock: 9, ubicacion: '', precio: ''},
-  {codigo: '2000/6', descripcion: 'TOPE CAPOT FIAT', stock: 10, ubicacion: '', precio: ''},
-  {codigo: '982108347ZD', descripcion: 'TERMINACION BAUL PEUGEOT', stock: 1, ubicacion: '', precio: ''},
-  {codigo: 'SOPRANGER', descripcion: 'SOPORTE PARAGOLPE TRASERO RANGER', stock: 6, ubicacion: '', precio: ''},
-  {codigo: 'WP9318', descripcion: 'FILTRO DE HABITÁCULO PEUGEOT/CITROEN', stock: 1, ubicacion: '', precio: ''},
-  {codigo: 'WP8440', descripcion: 'PASTILLA DE FRENO KANGOO', stock: 2, ubicacion: '', precio: ''},
-  {codigo: 'WP9286', descripcion: 'FILTRO FIAT PALIO', stock: 1, ubicacion: '', precio: ''},
-  {codigo: 'WP9166', descripcion: 'FILTRO VOLKSWAGEN', stock: 3, ubicacion: '', precio: ''},
-  {codigo: '0986 BFO 587', descripcion: 'FILTRO FORD KA', stock: 2, ubicacion: '', precio: ''},
-  {codigo: 'WP6928', descripcion: 'FILTRO KANGOO', stock: 2, ubicacion: '', precio: ''},
-  {codigo: '0986 BFO 589', descripcion: 'FILTRO FORD ECOSPORT', stock: 1, ubicacion: '', precio: ''},
-  {codigo: 'AKX35285', descripcion: 'FILTRO OPEL ZAFIRA', stock: 2, ubicacion: '', precio: ''}
+  {codigo: "1145", descripcion: "SOPORTE CAJA CHEVROLET CORSA", stock: 2, ubicacion: "DEPÓSITO 1, PASILLO 3", precio: "1"},
+  {codigo: "1146", descripcion: "SOPORTE MOTOR CHEVROLET CORSA LADO DISTRIBUCIÓN", stock: 2, ubicacion: "DEPÓSITO 1, PASILLO 3", precio: ""},
+  {codigo: "1147", descripcion: "SOPORTE MOTOR CHEVROLET CORSA LADO CAJA", stock: 3, ubicacion: "DEPÓSITO 1, PASILLO 3", precio: ""},
+  {codigo: "1148", descripcion: "SOPORTE MOTOR CHEVROLET CORSA TRASERO", stock: 2, ubicacion: "DEPÓSITO 1, PASILLO 3", precio: ""},
+  {codigo: "WP9318", descripcion: "FILTRO DE HABITÁCULO PEUGEOT/CITROEN", stock: 1, ubicacion: "LOCAL, OFICINA", precio: ""},
+  {codigo: "WP8444", descripcion: "PASTILLA DE FRENO KANGOO/ PARTNER", stock: 1, ubicacion: "LOCAL, OFICINA", precio: ""},
+  {codigo: "WP9286", descripcion: "FILTRO DE HABITÁCULO FIAT PALIO/SIENA/STRADA", stock: 2, ubicacion: "DEPÓSITO 1", precio: ""},
+  {codigo: "WP9166", descripcion: "FILTRO DE HABITÁCULO VOLKSWAGEN GOL TREND", stock: 1, ubicacion: "LOCAL, OFICINA", precio: ""},
+  {codigo: "WP9366", descripcion: "FILTRO DE HABITÁCULO VOLKSWAGEN AMAROK/TRANSPORTER", stock: 2, ubicacion: "DEPÓSITO 1", precio: ""},
+  {codigo: "WP19930", descripcion: "PASTILLA DE FRENO DELANTERO FIAT UNO/VIVACE", stock: 2, ubicacion: "LOCAL, OFICINA", precio: ""},
+  {codigo: "WP18350P", descripcion: "PASTILLA DE FRENO DELANTERO TOYOTA ETIOS", stock: 1, ubicacion: "LOCAL, ESTANTERÍA 2", precio: ""},
+  {codigo: "WP16330P", descripcion: "PASTILLA DE FRENO TRASERO COROLLA 06-", stock: 1, ubicacion: "LOCAL, ESTANTERÍA 2", precio: ""},
+  {codigo: "WP9900", descripcion: "PASTILLA DE FRENO DELANTERO PEUGEOT/CITROEN/RENAULT", stock: 1, ubicacion: "LOCAL, ESTANTERÍA 2", precio: ""},
+  {codigo: "2000/2", descripcion: "TOPE APOYA CAPOT VW POLO 96/00", stock: 1, ubicacion: "LOCAL, MOSTRADOR 1, CAJÓN 3", precio: ""},
+  {codigo: "2000/5", descripcion: "TOPE APOYA CAPOT PEUGEOT 306 95/97", stock: 1, ubicacion: "LOCAL, MOSTRADOR 1, CAJÓN 3", precio: ""},
+  {codigo: "2000/6", descripcion: "TOPE APOYA CAPOT FIAT", stock: 10, ubicacion: "LOCAL, MOSTRADOR 1, CAJÓN 3", precio: ""},
+  {codigo: "98210834ZD", descripcion: "TERMINACIÓN BAUL PEUGEOT 208 15/19 (GOOGLE)", stock: 1, ubicacion: "LOCAL, MOSTRADOR 1", precio: ""},
+  {codigo: "2000/4", descripcion: "TOPE APOYA CAPOT RENAULT 19 93/00", stock: 1, ubicacion: "LOCAL, MOSTRADOR 1", precio: ""},
+  {codigo: "SOPRANGER", descripcion: "SOPORTE PARAGOLPE TRASERO RANGER 2016", stock: 1, ubicacion: "LOCAL, ESTANTERÍA 2", precio: ""},
+  {codigo: "AKX 35285", descripcion: "FILTRO DE HABITÁCULO OPEL ZAFIRA", stock: 0, ubicacion: "DEPÓSITO 1, ESTANTERÍA 1", precio: ""}
 ];
 
 export default productos;
