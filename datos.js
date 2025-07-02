aad3feff88d7fa51c5b2b20bb13605216011bfbf
biknet1 <biknet.autopartes@gmail.com>
2025-07-02 00:36:36 -0300

    Create datos.js

---
+++ datos.js
@@ -0,0 +1,44 @@
+const productos = [
+  {
+    "codigo": "Z9Y",
+    "descripcion": "BUJIA DE ENCENDIDO MOTOS 110/125/250 (CHAMPION) Precio Mercado Libre",
+    "ubicacion": "LOCAL, MOSTRADOR 1, CAJON 2.",
+    "proveedor": "",
+    "precio": "",
+    "stock": 7
+  },
+  {
+    "codigo": "36862EX",
+    "descripcion": "KIT SOPORTES RADIADOR FOX/SURAN/GOL TREND EXTERIOR",
+    "ubicacion": "DEPTO 2, PASILLO1,ESTANTERIA 1, ESTANTE C CAJA Nº 15",
+    "proveedor": "ML",
+    "precio": "",
+    "stock": 2
+  },
+  {
+    "codigo": "898D",
+    "descripcion": "OPTICA IZQUIERDA KANGOO 08/12 COMANDO ELECTRICO SIN MOTOR",
+    "ubicacion": "DEPTO 1, HAB 2, ESTANTERIA LADO PUERTA",
+    "proveedor": "CROMOSOL",
+    "precio": "",
+    "stock": 1
+  },
+  {
+    "codigo": "5176/2",
+    "descripcion": "MOLDURA REJILLA RENAULT LOGAN 10/3 CAPOT SUP. CROMADA",
+    "ubicacion": "DEPTO 2, COCINA, ESTANTERIA 1, CAJON Nº12",
+    "proveedor": "CROMOSOL",
+    "precio": "",
+    "stock": 1
+  },
+  {
+    "codigo": "a0765",
+    "descripcion": "PARAGOLPE DELANTERO LOGAN F2 10/13",
+    "ubicacion": "DEPTO 2, HABITACION 2, Nº 2",
+    "proveedor": "CROMOSOL",
+    "precio": "",
+    "stock": 1
+  }
+];
+
+export default productos;
